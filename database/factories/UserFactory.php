24200e6c2633ee683a5afe1e675300faf2bb48b0
AhmadiDev21 <AhmadiDev21@gmail.com>
2021-01-27 06:57:26 -0800

    change user factory and add faker_locale

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'type'=>User::TYPE_USER,
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => '$2y$10$xdJPZM0AE9eltzDofHkWuOdr1TLFv/Up35bRbthHH6SRecUvURWzK', // 123456
+            'mobile'=> '+989'.random_int(1111, 9999).random_int(11111, 99999),
+            'avatar'=>null,
+            'website'=>$this->faker->url,
+            'verify_code'=>null,
+            'verified_at' => now(),
         ];
     }
 }
