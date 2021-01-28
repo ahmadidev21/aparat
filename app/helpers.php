f463042a17932de1a0329ac21fb054aa0eb8b1c9
AhmadiDev21 <AhmadiDev21@gmail.com>
2021-01-28 15:07:56 -0800

    change helper function format

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,21 @@
 /**
  * اضافه کردن +98 به ابتدای شماره موبایل
  */
-function to_valid_mobile_number($mobile)
-{
-    return '+98' . substr($mobile, -10, 10);
+if (! function_exists('to_valid_mobile_number')) {
+    function to_valid_mobile_number($mobile)
+    {
+        return '+98' . substr($mobile, -10, 10);
+    }
 }
+
+/*
+ * ایجاد کد فعال سازی تصادفی برای ثبت نام
+ */
+if (! function_exists('random_verification_code')) {
+    function random_verification_code()
+    {
+        return random_int(100000, 999999);
+    }
+}
+
+
