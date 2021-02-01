d3068677aab5432c9014805220049bcf490109f6
AhmadiDev21 <AhmadiDev21@gmail.com>
2021-02-01 14:04:03 -0800

    add playlist model

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -12,4 +12,9 @@ class Playlist extends Model
     protected $table = 'playlists';
 
     protected $fillable =['user_id', 'title'];
+
+    public function videos()
+    {
+        return $this->belongsToMany(Video::class, 'playlist_videos');
+    }
 }
