8911c969f9be191b05af98087450ede20d67ed04
AhmadiDev21 <AhmadiDev21@gmail.com>
2021-02-04 17:15:55 -0800

    solve the problem of create category

--- app/Http/Requests/Category/CreateCategoryRequest.php
+++ app/Http/Requests/Category/CreateCategoryRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Category;
 
+use App\Rules\UniqueForUser;
 use App\Rules\UploadedCategoryBannerId;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -25,7 +26,7 @@ class CreateCategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'=>'required|string|min:2|max:100|unique:categories,title',
+            'title'=>['required', 'string','min:2','max:100', new UniqueForUser('categories')],
             'icon'=>'nullable|string',//TODO: Certain use which icon
             'banner'=>['nullable', new UploadedCategoryBannerId()]
         ];
